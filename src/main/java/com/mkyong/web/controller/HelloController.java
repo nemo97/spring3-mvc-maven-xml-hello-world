023187ab09b2efddcda2d901a2ce633b8f5faf52
Subhas <subhas_sing@live.com>
2016-10-24 10:55:28 -0700

    not working in jboss5

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -7,13 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HelloController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-
+	public String printWelcome(ModelMap model,HttpServletRequest req) {
+		req.getSession().setAttribute("trest","test");
 		model.addAttribute("message", "Spring 3 MVC Hello World");
+
+		System.out.println(" session id "+req.getSession().getId()+"... "+req.getSession().getAttribute("trest"));
 		return "hello";
 
 	}
